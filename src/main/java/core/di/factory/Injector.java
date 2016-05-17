94e2740ec2aaf833139e38fd605bb9b2ac538595
rhyno.j <rhyno.j@kakaocorp.com>
2016-05-17 20:57:21 +0900

    12.1.4 중복 제거

--- src/main/java/core/di/factory/Injector.java
+++ src/main/java/core/di/factory/Injector.java
@@ -1,6 +1,7 @@
 package core.di.factory;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import core.annotation.Inject;
 import core.di.InjectorUtils;
 import org.reflections.ReflectionUtils;
@@ -16,36 +17,20 @@ import java.util.*;
 
 public interface Injector {
     void inject(Class<?> beanFrame);
-    int priority();
 }
 
-class DefaultListableInjector extends AbstractInjector {
-
+class DefaultListableInjector implements Injector {
     private final List<Injector> injectorList;
-
     public DefaultListableInjector(BeanFactory beanFactory) {
-        this(beanFactory, Arrays.asList(new Injector[]{
+        this(Arrays.asList(new Injector[]{
                 new ConstructorInjector(beanFactory),
                 new SetterInjector(beanFactory),
                 new DirectFieldInjector(beanFactory)
         }));
     }
 
-    public DefaultListableInjector(BeanFactory beanFactory, List<Injector> injectorList) {
-        super(beanFactory);
-        reorder(injectorList);
+    public DefaultListableInjector(List<Injector> injectorList) {
         this.injectorList = injectorList;
-    }
-
-    private void reorder(List<Injector> injectorList) {
-        Collections.sort(injectorList, (o1, o2) -> {
-            if (o1.priority() > o2.priority())
-                return -1;
-            else if (o1.priority() == o2.priority())
-                return 0;
-            else
-                return 1;
-        });
     }
 
     @Override
@@ -55,13 +40,26 @@ class DefaultListableInjector extends AbstractInjector {
 }
 
 abstract class AbstractInjector implements Injector {
-
     private final BeanFactory beanFactory;
     protected final Logger log = LoggerFactory.getLogger(AbstractInjector.class);
 
     public AbstractInjector(BeanFactory beanFactory) {
         this.beanFactory = beanFactory;
     }
+
+    @Override
+    public void inject(Class<?> beanFrame) {
+        Object bean = instantiateClass(beanFrame);
+        Set<?> targets = getInjectTargets(beanFrame);
+
+        for(Iterator<?> iterator = targets.iterator(); iterator.hasNext();) {
+            inject(iterator.next(), bean);
+        }
+    }
+
+    protected abstract Set<?> getInjectTargets(Class<?> beanFrame);
+
+    protected abstract void inject(Object target, Object bean);
 
     protected Object instantiateClass(Class<?> clazz) {
         Object bean = beanFactory.getBean(clazz);
@@ -100,11 +98,6 @@ abstract class AbstractInjector implements Injector {
         }
         return BeanUtils.instantiateClass(constructor, args.toArray());
     }
-
-    @Override
-    public int priority() {
-        return 2;
-    }
 }
 
 class ConstructorInjector extends AbstractInjector {
@@ -114,14 +107,14 @@ class ConstructorInjector extends AbstractInjector {
     }
 
     @Override
-    public void inject(Class<?> beanFrame) {
-        instantiateClass(beanFrame);
+    protected Set<Constructor> getInjectTargets(Class<?> beanFrame) {
+        Set<Constructor> set = Sets.newHashSet();
+        set.add(InjectorUtils.getInjectedConstructor(beanFrame));
+        return set;
     }
 
     @Override
-    public int priority() {
-        return 1;
-    }
+    protected void inject(Object target, Object bean) {}
 }
 
 class SetterInjector extends AbstractInjector {
@@ -131,37 +124,31 @@ class SetterInjector extends AbstractInjector {
     }
 
     @Override
-    public void inject(Class<?> beanFrame) {
-        Object bean = instantiateClass(beanFrame);
-        Set<Method> methods = ReflectionUtils.getMethods(beanFrame, ReflectionUtils.withAnnotation(Inject.class));
-
-        for(Iterator<Method> iterator = methods.iterator(); iterator.hasNext();) {
-            Method method = iterator.next();
-            method.setAccessible(true);
-            switch (method.getParameters().length) {
-                case 0:
-                    throw new UnsupportedOperationException("set method parameter must be exist");
-                case 1:
-                    break;
-                default:
-                    throw new UnsupportedOperationException("set method can not have multiple parameters");
-
-            }
-            try {
-                method.invoke(bean, new Object[]{instantiateClass(method.getParameters()[0].getType())});
-            } catch (IllegalAccessException e) {
-                log.error("Cannot access to Method. because Method is not public");
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                log.error("Cannot access to Method. Bean DI is Fail");
-                e.printStackTrace();
-            }
-        }
+    protected Set<?> getInjectTargets(Class<?> beanFrame) {
+        return ReflectionUtils.getMethods(beanFrame, ReflectionUtils.withAnnotation(Inject.class));
     }
 
     @Override
-    public int priority() {
-        return 2;
+    protected void inject(Object target, Object bean) {
+        Method method = (Method) target;
+        method.setAccessible(true);
+        switch (method.getParameters().length) {
+            case 0:
+                throw new UnsupportedOperationException("set method parameter must be exist");
+            case 1:
+                break;
+            default:
+                throw new UnsupportedOperationException("set method can not have multiple parameters");
+        }
+        try {
+            method.invoke(bean, new Object[]{instantiateClass(method.getParameters()[0].getType())});
+        } catch (IllegalAccessException e) {
+            log.error("Cannot access to Method. because Method is not public");
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            log.error("Cannot access to Method. Bean DI is Fail");
+            e.printStackTrace();
+        }
     }
 }
 
@@ -171,23 +158,19 @@ class DirectFieldInjector extends AbstractInjector {
     }
 
     @Override
-    public void inject(Class<?> beanFrame) {
-        Object bean = instantiateClass(beanFrame);
-        Set<Field> fields = ReflectionUtils.getAllFields(beanFrame, ReflectionUtils.withAnnotation(Inject.class));
-        for(Iterator<Field> iterator = fields.iterator(); iterator.hasNext();) {
-            Field field = iterator.next();
-            field.setAccessible(true);
-            try {
-                field.set(bean, instantiateClass(field.getType()));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-                log.error("Cannot access to field. Bean DI is Fail");
-            }
-        }
+    protected Set<?> getInjectTargets(Class<?> beanFrame) {
+        return ReflectionUtils.getAllFields(beanFrame, ReflectionUtils.withAnnotation(Inject.class));
     }
 
     @Override
-    public int priority() {
-        return 3;
+    protected void inject(Object target, Object bean) {
+        Field field = (Field) target;
+        field.setAccessible(true);
+        try {
+            field.set(bean, instantiateClass(field.getType()));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            log.error("Cannot access to field. Bean DI is Fail");
+        }
     }
 }
